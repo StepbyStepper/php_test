69b51624ee96a379cd06f140c7f6efbf6b2b8081
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2018-12-24 20:46:17 +0300

    Init

---
+++ controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php 
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    public function actionTest()
+    {
+        $user = User::findOne(1);
+        
+        Yii::$app->user->login($user);
+        var_dump(Yii::$app->components); die;
+    }
+}
---
+++ migrations/m181219_133548_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m181219_133548_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'email' => $this->string()->defaultValue(null),
+            'password' => $this->integer(),
+            'isAdmin' => $this->integer()->defaultValue(0),
+            'photo'=> $this->string()->defaultValue(null)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('user');
+    }
+}
